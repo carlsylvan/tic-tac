865046f98ddb5ed379c5bfa1a326940015760682
carlsylvan <carlsylvan@gmail.com>
2022-11-02 13:55:40 +0100

    added to-do list

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,20 @@
-
-
 class Square {
-    constructor (color1, color2, id) {
+    constructor (id) {
         this.id = id;
-        this.color1 = color1;
-        this.color2 = color2;
     }
 }
 
 let squares = [];
 
 for (let i = 0; i < 9; i++) {
-    let newSquare = new Square ("red", "green", (i + 1));
+    let newSquare = new Square (i + 1);
     squares.push(newSquare);
 } 
-
-function handleClick(square) {
-    console.log("Du klickade på ruta nummer " + square.id);
-    document.getElementById("square" + (square.id)).style.backgroundColor = "red"; 
-}
-
 
 for (let i = 0; i < squares.length; i++) {
     let square = document.createElement("div");
     square.id = "square" + (i + 1);
+    square.classList = "square";
     square.innerHTML = i + 1;
     document.body.appendChild(square);
 
@@ -31,3 +22,27 @@ for (let i = 0; i < squares.length; i++) {
         handleClick(squares[i])
     })
 }
+
+// let announcerBox = document.createElement("span");
+// announcerBox.innerHTML = ""
+// document.body.appendChild(announcerBox);
+
+let plays = 0;
+
+function handleClick(square) {
+    if (plays % 2 === 0) {
+        document.getElementById("square" + (square.id)).style.backgroundColor = "red";
+        console.log("Spelare 1 klickade på ruta " + square.id);
+    } else if (plays % 2 === 1) {
+        document.getElementById("square" + (square.id)).style.backgroundColor = "green";
+        console.log("Spelare 2 klickade på ruta " + square.id);
+    }
+    plays += 1;
+
+    if (plays >= 9) {
+        alert("Spelet är slut");
+    }
+}
+
+// Lägg till spelarnas drag som lista
+// Lägg till winning conditions/end conditions som görs genom att loopa genom spelarnas gjorda drag i separat funktion
