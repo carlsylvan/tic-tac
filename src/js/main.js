39e8b8e696ce8ebc0f6443d562c8214105c0d89b
carlsylvan <carlsylvan@gmail.com>
2022-11-02 15:00:50 +0100

    new backgroundcolor

--- src/js/main.js
+++ src/js/main.js
@@ -27,22 +27,39 @@ for (let i = 0; i < squares.length; i++) {
 // announcerBox.innerHTML = ""
 // document.body.appendChild(announcerBox);
 
+let playerOnePlays = [];
+let playerTwoPlays = [];
 let plays = 0;
 
 function handleClick(square) {
     if (plays % 2 === 0) {
         document.getElementById("square" + (square.id)).style.backgroundColor = "red";
         console.log("Spelare 1 klickade på ruta " + square.id);
+        playerOnePlays.push(square.id);
+        console.log(playerOnePlays);
     } else if (plays % 2 === 1) {
         document.getElementById("square" + (square.id)).style.backgroundColor = "green";
         console.log("Spelare 2 klickade på ruta " + square.id);
+        playerTwoPlays.push(square.id);
+        console.log(playerTwoPlays);
     }
     plays += 1;
-
-    if (plays >= 9) {
-        alert("Spelet är slut");
-    }
+    winCondition();
 }
 
-// Lägg till spelarnas drag som lista
+function winCondition() {
+    if (playerOnePlays.includes()) {
+        alert("Spelare 1 vann");
+    }
+
+
+}
+
+
+// Lägg till spelarnas drag som lista (alla false först) - när spelaren klickar en viss ruta byts den till true 
 // Lägg till winning conditions/end conditions som görs genom att loopa genom spelarnas gjorda drag i separat funktion
+
+// Winningconditions = på i-nummer
+// 0 1 2, 3 4 5, 6 7 8,
+// 0 3 6, 1 4 7, 2 5 8,
+// 0 4 8, 2 4 6.
